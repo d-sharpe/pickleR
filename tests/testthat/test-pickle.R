5b1edb542aa43f8c1e7bec8b47ed26cba4b38555
d-sharpe <d.sharpe1@gmail.com>
2021-03-05 16:58:43 +0000

    initial test cases

--- tests/testthat/test-pickle.R
+++ tests/testthat/test-pickle.R
@@ -1,17 +1,18 @@
 library(pryr)
 
+numericVector <- runif(100)
+numericVectorAddress <- address(numericVector)
+
+integerVector <- 1L:100L
+integerVectorAddress <- address(integerVector)
+
+characterVector <- sample(LETTERS, 100, replace = TRUE)
+characterVectorAddress <- address(characterVector)
+
+logicalVector <- sample(c(TRUE, FALSE), 100, replace = TRUE)
+logicalVectorAddress <- address(logicalVector)
+
 test_that("pickle of base types works", {
-  numericVector <- runif(100)
-  numericVectorAddress <- address(numericVector)
-
-  integerVector <- 1L:100L
-  integerVectorAddress <- address(integerVector)
-
-  characterVector <- sample(LETTERS, 100, replace = TRUE)
-  characterVectorAddress <- address(characterVector)
-
-  logicalVector <- sample(c(TRUE, FALSE), 100, replace = TRUE)
-  logicalVectorAddress <- address(logicalVector)
 
   expect_vector((serializedPickle <- pickle(numericVector)))
   unserializedPickle <- unserialize(serializedPickle)
@@ -52,4 +53,138 @@ test_that("pickle of base types works", {
     unpickle(serializedPickle)
 
   expect_identical(logicalVector, unpickledValues)
+
+  mixedList <-
+    list(ItemA = numericVector,
+         ItemB = characterVector,
+         ItemC = integerVector)
+
+  expect_vector((serializedPickle <- pickle(mixedList)))
+
+  unpickledValues <-
+    unpickle(serializedPickle)
+
+  expect_identical(mixedList, unpickledValues)
+})
+
+test_that("pickle of object with attributes works", {
+
+  factorVector <-
+    factor(sample(LETTERS[1:5], 100, replace = T))
+
+  factorVectorAddress <- address(factorVector)
+
+  expect_vector((serializedPickle <- pickle(factorVector)))
+
+  unpickledValues <-
+    unpickle(serializedPickle)
+
+  expect_identical(factorVector, unpickledValues)
+})
+
+test_that("pickle of an environment works", {
+
+  env <-
+    new.env(parent = emptyenv())
+
+  env$a <-
+    numericVector
+
+  env$b <-
+    characterVector
+
+  expect_vector((serializedPickle <- pickle(env)))
+
+  unpickledValues <-
+    unpickle(serializedPickle)
+
+  expect_identical(names(env), names(unpickledValues))
+
+  expect_identical(env$a, unpickledValues$a)
+
+  expect_identical(env$b, unpickledValues$b)
+
+
+})
+
+
+test_that("pickle of a recursive environment works", {
+
+  temp_env <-
+    new.env(parent = emptyenv())
+
+  temp_env$a <-
+    numericVector
+
+  temp_env$b <-
+    characterVector
+
+  temp_env$c <-
+    list(temp_env)
+
+  expect_vector((serializedPickle <- pickle(temp_env)))
+
+  unpickledValues <-
+    unpickle(serializedPickle)
+
+  expect_identical(names(temp_env), names(unpickledValues))
+
+  expect_identical(temp_env$a, unpickledValues$a)
+
+  expect_identical(temp_env$b, unpickledValues$b)
+
+  expect_identical(unpickledValues$c, list(unpickledValues))
+
+  fun <-
+    function(...) { return(50)}
+
+  makeActiveBinding("d", fun, temp_env)
+
+  expect_vector((serializedPickle <- pickle(temp_env)))
+
+  unpickledValues <-
+    unpickle(serializedPickle)
+
+  expect_setequal(names(temp_env), names(unpickledValues))
+
+  expect_identical(temp_env$a, unpickledValues$a)
+
+  expect_identical(temp_env$b, unpickledValues$b)
+
+  expect_identical(unpickledValues$c, list(unpickledValues))
+
+  expect_identical(unpickledValues$d, 50)
+
+  expect_true(bindingIsActive("d", unpickledValues))
+
+  })
+
+
+test_that("pickle of a function works",  {
+
+  fun <-
+    function(a) {
+      a + 1
+    }
+
+  expect_vector((serializedPickle <- pickle(fun)))
+
+  unpickledValues <-
+    unpickle(serializedPickle)
+
+  expect_identical(formals(fun), formals(unpickledValues))
+
+  expect_identical(body(fun), body(unpickledValues))
+
+
+  environment(fun) <- new.env(parent = emptyenv())
+
+  expect_vector((serializedPickle <- pickle(fun)))
+
+  unpickledValues <-
+    unpickle(serializedPickle)
+
+  expect_identical(formals(fun), formals(unpickledValues))
+
+  expect_identical(body(fun), body(unpickledValues))
 })
